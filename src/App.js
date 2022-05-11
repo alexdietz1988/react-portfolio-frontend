3072705c7a8b0807630587c1d42c50b4ca423013
alexdietz1988 <alex.dietz@gmail.com>
2022-05-11 13:38:03 -0400

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
+  const URL = 'https://alexdietz-react-portfolio.herokuapp.com'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/projects'>
+          <Projects URL={URL} />
+        </Route>
+        <Route path='/about'>
+          <About URL={URL} />
+        </Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+function About(props) {
+
+    const [about, setAbout] = useState(null)
+
+    async function getAboutData() {
+        const response = await fetch(props.URL + 'about')
+        const data = await response.json()
+        setAbout(data)
+    }
+
+    useEffect(() => getAboutData(), [])
+
+    function loaded() {
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    }
+
+    return about ? loaded() : <h1>Loading...</h1>
+}
+
+export default About
